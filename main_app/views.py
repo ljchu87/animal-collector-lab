0075992c39a5a5a9b2ebda39d455ce7347ed9164
Leon Chu <Ljchu87@gmail.com>
2022-11-05 14:55:57 -0500

    Initialize Animals model and import into views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,13 @@
 from django.shortcuts import render
-
-# Add the following import
-from django.http import HttpResponse
-
-# Add the Cat class & list and view function below the imports
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-animals = [
-  Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-  Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+from .models import Animals
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def animals_index(request):
+  animals = Animals.objects.all()
   return render(request, 'animals/index.html', { 'animals': animals })
