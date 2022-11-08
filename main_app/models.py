6da12b56f5f8f5a378838167f9c2eccba2dcc194
Leon Chu <Ljchu87@gmail.com>
2022-11-07 20:43:00 -0600

    Initialize delete an animal

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Animals(models.Model):
@@ -9,3 +10,6 @@ class Animals(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('animals_detail', kwargs={'animal_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Animals
 
 # Define the home view
@@ -20,3 +20,13 @@ def animals_detail(request, animal_id):
 class AnimalCreate(CreateView):
   model = Animals
   fields = '__all__'
+  success_url = '/animals/'
+
+class AnimalUpdate(UpdateView):
+  model = Animals
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class AnimalDelete(DeleteView):
+  model = Animals
+  success_url = '/animals/'
