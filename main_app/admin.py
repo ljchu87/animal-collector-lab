52bf01e24063de6452bcac97e7fc376eb6eb6e92
Leon Chu <Ljchu87@gmail.com>
2022-11-09 14:25:06 -0600

    Add feeding model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import your models here
-from .models import Animals
+from .models import Animals, Feeding
 
 # Register your models here
 admin.site.register(Animals)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 # Create your models here.
 class Animals(models.Model):
   name = models.CharField(max_length=100)
@@ -13,3 +19,16 @@ class Animals(models.Model):
 
   def get_absolute_url(self):
     return reverse('animals_detail', kwargs={'animal_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  animal = models.ForeignKey(Animals, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
